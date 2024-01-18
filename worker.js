09693eff73d24c843da5e5d2fd9dbc7b07ee1154
jassu1992 <jassu.nagar65@gmail.com>
2024-01-18 15:13:18 +0530

    update the worker js

--- worker.js
+++ worker.js
@@ -5,27 +5,22 @@ onconnect = (e) => {
     const port = e.ports[0];
     browserInstance.push(port)
     // console.log('--on connect method of the worker js--', port)
-    // console.log('--worker js onmessage outside clicked--: ')
     
     port.onmessage = ({data}) => {
         // debugger
-        console.log('---inside the worker js file onmessage---', data)
-        // switch(data.mutation) {
-        //     case 'RESTART':
-        //         console.log('--inside the worker js switch ADD --', data)
-        //         postMessage({ mutation: 'RESTART', value: data.value });
-        //         break
-        //     default:
-        //         console.log('---inside the default switch case of worker js--')
-        // }
-        // port.postMessage(data)
-       
+       // console.log('---inside the worker js file onmessage---', data)
+        switch(data.mutation) {
+            case 'RESTART':
+                postMessage(data)
+                break
+            default:
+                postMessage({mutation: 'STOP', value: 1})
+        }       
     };
 };
 
 function postMessage(data) {
-    console.log('---inside the post message fun or worker js file')
     browserInstance.map(instance => {
-        instance.postMessage({data})
+        instance.postMessage(data)
     })
 }
